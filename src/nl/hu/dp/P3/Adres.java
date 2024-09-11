13fb2ecee1495bd1e4c538fcc857f47f8cbe855a
OluwaseyiDavies <oluwaseyi.davies@student.hu.nl>
2024-09-11 13:18:13 +0200

    De klasse Adres aangemaakt

---
+++ src/nl/hu/dp/P3/Adres.java
@@ -0,0 +1,70 @@
+package nl.hu.dp.P3;
+
+import nl.hu.dp.P2.domain.Reiziger;
+
+public class Adres {
+    private int adres_id;
+    private String huisnummer;
+    private String straat;
+    private String woonplaats;
+    private Reiziger reiziger;
+
+    public Adres(int adres_id, String huisnummer, String straat, String woonplaats, Reiziger reiziger) {
+        this.adres_id = adres_id;
+        this.huisnummer = huisnummer;
+        this.straat = straat;
+        this.woonplaats = woonplaats;
+        this.reiziger = reiziger;
+    }
+
+    public int getAdres_id() {
+        return adres_id;
+    }
+
+    public void setAdres_id(int adres_id) {
+        this.adres_id = adres_id;
+    }
+
+    public String getHuisnummer() {
+        return huisnummer;
+    }
+
+    public void setHuisnummer(String huisnummer) {
+        this.huisnummer = huisnummer;
+    }
+
+    public String getStraat() {
+        return straat;
+    }
+
+    public void setStraat(String straat) {
+        this.straat = straat;
+    }
+
+    public String getWoonplaats() {
+        return woonplaats;
+    }
+
+    public void setWoonplaats(String woonplaats) {
+        this.woonplaats = woonplaats;
+    }
+
+    public Reiziger getReiziger() {
+        return reiziger;
+    }
+
+    public void setReiziger(Reiziger reiziger) {
+        this.reiziger = reiziger;
+    }
+
+    @Override
+    public String toString() {
+        return "Adres{" +
+                "adres_id=" + adres_id +
+                ", huisnummer='" + huisnummer + '\'' +
+                ", straat='" + straat + '\'' +
+                ", woonplaats='" + woonplaats + '\'' +
+                ", reiziger=" + reiziger.getId() +
+                '}';
+    }
+}
