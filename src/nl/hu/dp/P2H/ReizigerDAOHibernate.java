757882677e1191862a02326f5a245db025e8351a
OluwaseyiDavies <oluwaseyi.davies@student.hu.nl>
2024-09-16 10:42:53 +0200

    P2H ReizigerDAO aangemaakt

---
+++ src/nl/hu/dp/P2H/ReizigerDAOHibernate.java
@@ -0,0 +1,75 @@
+package nl.hu.dp.P2H;
+
+import nl.hu.dp.P2H.domain.Reiziger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import java.util.List;
+
+public class ReizigerDAOHibernate implements ReizigerDAO {
+    private Session session;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
