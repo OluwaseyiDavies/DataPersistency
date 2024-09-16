f3dd15b582fa1d74972462b99476548d61843636
OluwaseyiDavies <oluwaseyi.davies@student.hu.nl>
2024-09-16 10:50:01 +0200

    ReizigerDAOHibernate uitgewerkt

--- src/nl/hu/dp/P2H/ReizigerDAOHibernate.java
+++ src/nl/hu/dp/P2H/ReizigerDAOHibernate.java
@@ -7,6 +7,64 @@ import java.util.List;
 
 public class ReizigerDAOHibernate implements ReizigerDAO {
     private Session session;
+
+    public ReizigerDAOHibernate(Session session) {
+        this.session = session;
+    }
+
+    @Override
+    public boolean save(Reiziger reiziger) {
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.save(reiziger);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean update(Reiziger reiziger) {
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.update(reiziger);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override boolean delete(Reiziger reiziger) {
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.delete(reiziger);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public Reiziger findById(int id) {
+        return session.get(Reiziger.class, id);
+    }
+
+    @Override
+    public List<Reiziger> findAll() {
+        return session.createQuery("FROM Reiziger", Reiziger.class).list();
+    }
 }
 
 
